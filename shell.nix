21f27528ea8c5c27d6f1cf542819d46ce9ce1cab
Lucas Pickering <lucas@lucaspickering.me>
2022-02-23 18:38:44 -0500

    Working django server

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+let
+  python-with-my-packages = python310.withPackages (p: with p; [
+  ]);
+in
+
+mkShell {
+  buildInputs = [
+    clang
+    gnumake
+    lld
+    pkg-config
+    python310
+    poetry
+    zlib.dev
+  ];
+
+  shellHook = ''
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+  '';
+}
