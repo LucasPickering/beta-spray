9cfaff0a0a8257032b51ee0f1b7e24b64240f51e
Lucas Pickering <lucas@lucaspickering.me>
2022-03-29 20:08:18 -0400

    Fix GKE resource warnings

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 ### Ops
 
-- Fix resource warning from Kube
 - DB PVC
 - Documentation
 
--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -4,4 +4,11 @@ set -e
 
 VERSION=$(git rev-parse HEAD)
 
-helm upgrade beta-spray helm/ --set version_sha=$VERSION
+if $(helm list | grep -q beta-spray); then
+    SUBCOMMAND=upgrade
+else
+    SUBCOMMAND=install
+fi
+
+set -x
+helm $SUBCOMMAND beta-spray helm/ --set version_sha=$VERSION
