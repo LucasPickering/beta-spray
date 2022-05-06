486452516992cf692079d11ed2188f1940397172
Lucas Pickering <lucas@lucaspickering.me>
2022-05-06 10:27:36 -0400

    Fix css bug on editor sidebar

--- ui/src/components/Editor/EditorControls/EditorPanel.tsx
+++ ui/src/components/Editor/EditorControls/EditorPanel.tsx
@@ -6,7 +6,13 @@ import EditHoldsButton from "./EditHoldsButton";
  * Static container for editor controls. For large screens.
  */
 const EditorPanel: React.FC = ({ children }) => (
-  <Stack direction="column" spacing={2} padding={2} overflow="auto">
+  <Stack
+    direction="column"
+    spacing={2}
+    padding={2}
+    overflow="auto"
+    flexShrink={0}
+  >
     <EditHoldsButton />
 
     {children}
