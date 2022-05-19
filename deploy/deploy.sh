f2f7e887dd96dda85ac279c37360d8d4c83531c8
Lucas Pickering <lucas@lucaspickering.me>
2022-05-19 11:25:00 -0400

    Auto-tag deployed version in git

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+GIT_TAG=latest
 REPO_ROOT=$(git rev-parse --show-toplevel)
 VERSION_SHA=$(git rev-parse origin/master)
 
-echo "Deploying version $VERSION_SHA"
+echo "Deploying version '$VERSION_SHA'"
 terraform -chdir=$REPO_ROOT/deploy/terraform/deploy apply -var version_sha=$VERSION_SHA
+
+# Update
+git tag -f $GIT_TAG $VERSION_SHA
+git push -f origin $GIT_TAG
+echo "Updated git tag '$GIT_TAG' to '$VERSION_SHA'"
