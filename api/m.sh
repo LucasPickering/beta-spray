cd0a0734bb6ac37f076145014a1dc97f3c3581f9
Lucas Pickering <lucas@lucaspickering.me>
2022-05-20 07:21:15 -0400

    Add script to load prod data

--- api/m.sh
+++ api/m.sh
@@ -3,8 +3,8 @@
 # Run a manage.py command. If not in the api container already, this will exec into it
 
 if test -f "/.dockerenv" || [ -n "$KUBERNETES_SERVICE_HOST" ]; then
-    cd src && poetry run ./manage.py $@
+    cd src && poetry run ./manage.py "$@"
 else
     # Not in docker yet, re-run the command within the container
-    docker exec -it $(docker-compose ps -q api) ./m.sh $@
+    docker exec -it $(docker-compose ps -q api) ./m.sh "$@"
 fi
