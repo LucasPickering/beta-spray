584128c53fcb2c19053e298a277cf4ff3aa7f52a
Lucas Pickering <lucas@lucaspickering.me>
2022-06-13 12:24:33 -0400

    Replace seaorm with sqlx

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DATABASE_URL").unwrap())
+        .await?;
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let row = sqlx::query!("SELECT $1 as ass", 1_i64)
+        .fetch_one(&pool)
+        .await?;
+
+    dbg!(row);
+
+    Ok(())
 }
