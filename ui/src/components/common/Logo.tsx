4172bccccd75be57018db7eb0506e81cc0033022
Lucas Pickering <lucas@lucaspickering.me>
2022-07-24 18:57:36 -0400

    Add alpha badge

--- ui/src/components/common/Logo.tsx
+++ ui/src/components/common/Logo.tsx
@@ -6,11 +6,18 @@ import { IconLogo } from "./icons";
  * The full site logo, including the name
  */
 const Logo: React.FC = () => (
-  <Typography variant="h5" component="h1" display="flex" alignItems="center">
+  <Box display="flex" alignItems="center">
     <IconLogo color="primary" />
-
-    <Box marginLeft={1}>Beta Spray</Box>
-  </Typography>
+    <Typography variant="h5" component="h1" marginLeft={1}>
+      Beta Spray
+    </Typography>
+    <Typography
+      variant="caption"
+      sx={{ transform: "translate(-6px, -5px) rotate(45deg)" }}
+    >
+      alpha
+    </Typography>
+  </Box>
 );
 
 export default Logo;
