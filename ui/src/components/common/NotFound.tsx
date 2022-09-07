ae0413d8e4ca4633c778becc8f693566966302ba
Lucas Pickering <lucas@lucaspickering.me>
2022-09-07 09:17:41 -0400

    Improve 404 page

--- ui/src/components/common/NotFound.tsx
+++ ui/src/components/common/NotFound.tsx
@@ -1,5 +1,23 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
-const NotFound: React.FC = () => <span>Not Found :(</span>;
+const NotFound: React.FC = () => (
+  <Box
+    width="100%"
+    height="100%"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+  >
+    <div>
+      <Typography variant="h2" component="div" textAlign="center">
+        ¯\_(ツ)_/¯
+      </Typography>
+      <Typography variant="h4" component="div" textAlign="center">
+        Not Found
+      </Typography>
+    </div>
+  </Box>
+);
 
 export default NotFound;
