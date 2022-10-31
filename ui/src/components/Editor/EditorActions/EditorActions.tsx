91b7532b968393c9e6b9e348026c33588745b1cb
Lucas Pickering <lucas@lucaspickering.me>
2022-10-31 16:02:55 -0400

    Switch to speed dial for beta move actions

--- ui/src/components/Editor/EditorActions/EditorActions.tsx
+++ ui/src/components/Editor/EditorActions/EditorActions.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Box } from "@mui/material";
 
 interface Props {
   children?: React.ReactNode;
@@ -8,25 +8,7 @@ interface Props {
  * Wrapper for the action buttons that appear at the bottom the screen.
  */
 const EditorActions: React.FC<Props> = ({ children }) => (
-  <Stack
-    direction="row"
-    spacing={1}
-    sx={{
-      position: "absolute",
-      bottom: 0,
-      width: "100%",
-      maxWidth: 400,
-      padding: 1,
-      justifyContent: "center",
-
-      // Size all children equally
-      "& > *": {
-        flexGrow: 1,
-      },
-    }}
-  >
-    {children}
-  </Stack>
+  <Box sx={{ position: "absolute", bottom: 16, right: 16 }}>{children}</Box>
 );
 
 export default EditorActions;
