e2e3d7040c3fab6d290deef079c6e814ad4dd614
Lucas Pickering <lucas@lucaspickering.me>
2023-03-07 08:32:52 -0500

    Fix admin UI in kube namespaces
    We needed to tell Django that the X-Forwarded-Proto header is safe to trust (which it is since nginx overwrites it)

--- api/src/beta_spray/settings/settings_prd.py
+++ api/src/beta_spray/settings/settings_prd.py
@@ -4,6 +4,13 @@ from .settings import *  # noqa: F401,F403
 
 BETA_SPRAY_HOSTNAME = os.getenv("BETA_SPRAY_HOSTNAME")
 ALLOWED_HOSTS = ["localhost", "api", BETA_SPRAY_HOSTNAME]
+
+# WARNING: This is only save if nginx is configured to strip the
+# X-Forwarded-Proto and replace it with its own value. Needed to make CSRF work.
+# https://docs.djangoproject.com/en/4.0/ref/settings/#secure-proxy-ssl-header
+# https://stackoverflow.com/a/71482883/1907353
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 DEBUG = False
 SECRET_KEY = os.getenv("BETA_SPRAY_SECRET_KEY")
 # By not starting this URL with /api/, we avoid collisions between URL matches
