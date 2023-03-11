0a6bfc636ec1afcc446d37b2e0d01cd829097778
Lucas Pickering <lucas@lucaspickering.me>
2023-03-11 17:05:29 -0500

    Invalidate current user after guest is created

--- api/src/core/schema/mutation.py
+++ api/src/core/schema/mutation.py
@@ -1,8 +1,8 @@
 import random
 from typing import Annotated, NewType, Optional
 
-from django.forms import ValidationError, model_to_dict
 from django.contrib.auth import logout
+from django.forms import ValidationError, model_to_dict
 from strawberry.types.info import Info
 from strawberry_django_plus import gql
 from strawberry_django_plus.mutations import resolvers
