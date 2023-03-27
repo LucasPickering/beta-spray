1beaaab0a8e2492c1a74c4ef73f490a71e566b00
Lucas Pickering <lucas@lucaspickering.me>
2023-03-26 20:25:53 -0400

    Fix session when merging guest account into existing

--- api/src/core/pipeline.py
+++ api/src/core/pipeline.py
@@ -1,6 +1,8 @@
 from typing import Any
 
+from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.http import HttpRequest
 from guest_user.functions import get_guest_model, is_guest_user
 from social_core.backends.base import BaseAuth
 from social_core.exceptions import AuthAlreadyAssociated
@@ -9,7 +11,12 @@ from .models import Beta, Problem
 
 
 def find_existing_user(
-    backend: BaseAuth, uid: str, user: User = None, *args: Any, **kwargs: Any
+    backend: BaseAuth,
+    uid: str,
+    request: HttpRequest,
+    user: User = None,
+    *args: Any,
+    **kwargs: Any,
 ) -> dict:
     """
     A copy of the stock social_user pipeline, modified to allow linking guest
@@ -20,7 +27,7 @@ def find_existing_user(
     if social:
         if user and social.user != user:
             if is_guest_user(user):
-                # Link the guest into the existing user. THe guest user may
+                # Link the guest into the existing user. The guest user may
                 # own some objects, so we need to update those to point to the
                 # new owner. We'll have to add to this list any time we add
                 # `owner` columns, which kinda sucks
@@ -29,10 +36,17 @@ def find_existing_user(
 
                 user.delete()
                 user = social.user
-                # TODO this doesn't reassign the request session, so the user
-                # isn't logged in at the end of the request. Fix this.
+                # Update the request session to point to the logged-in user
+                backend_class = backend.__class__
+                login(
+                    request,
+                    user,
+                    # This is a bit jank but I can't figure out how else to get
+                    # the backend import path from the value
+                    f"{backend_class.__module__}.{backend_class.__name__}",
+                )
             else:
-                # This *shouldn't* happen, because it implies was already
+                # This *shouldn't* happen, because it implies user was already
                 # logged in under a regular account, then tried to log in again.
                 # The login screen should only be visible if the user is
                 # unauthenticated or a guest
