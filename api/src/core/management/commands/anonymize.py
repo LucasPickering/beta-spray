a90eeee57eb89eb673c920cd7518cebcf07a949b
Lucas Pickering <lucas@lucaspickering.me>
2023-04-23 15:32:05 -0400

    Shorten anonymized usernames

--- api/src/core/management/commands/anonymize.py
+++ api/src/core/management/commands/anonymize.py
@@ -3,6 +3,8 @@ from typing import Any
 from django.contrib.auth.models import User
 from django.contrib.postgres.functions import RandomUUID
 from django.core.management.base import BaseCommand
+from django.db.models import TextField
+from django.db.models.functions import Cast, Substr
 
 
 class Command(BaseCommand):
@@ -10,7 +12,8 @@ class Command(BaseCommand):
 
     def handle(self, **kwargs: Any) -> None:
         User.objects.update(
-            username=RandomUUID(),
+            # Use an abbreviated UUID, so it fits well in the UI
+            username=Substr(Cast(RandomUUID(), TextField()), 1, 8),
             email="",
             password="",
             first_name="",
