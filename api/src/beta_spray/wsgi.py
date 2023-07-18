6cb5a0859c7e48020900613ce473dabd2bc587a1
Lucas Pickering <lucas@lucaspickering.me>
2023-07-18 18:59:46 -0400

    Fix settings module in kube

--- api/src/beta_spray/wsgi.py
+++ api/src/beta_spray/wsgi.py
@@ -7,10 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beta_spray.settings")
 
 application = get_wsgi_application()
